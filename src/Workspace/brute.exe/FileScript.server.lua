b05e5cd77543ed33eb55c758908983de3c1d247f
darkp435 <duantiger19@gmail.com>
2025-06-05 20:48:09 +1200

    d

--- src/Workspace/brute.exe/FileScript.server.lua
+++
@@ -1,7 +0,0 @@
-local file = script.Parent
-local clickDetector = file:WaitForChild("ClickDetector")
-local getFile = require(game.ServerScriptService:WaitForChild("GetFile"))
-
-clickDetector.MouseClick:Connect(function(playerWhoClicked) 
-	getFile.EquipFile(playerWhoClicked, "brute.exe")
-end)
