e3a809f5b5ff22d4704a2eacef9f54db5391c2d7
darkp435 <duantiger19@gmail.com>
2025-06-07 17:43:11 +1200

    stuff

--- src/ServerStorage/brute.exe/SendSignal.client.lua
+++
@@ -1,5 +0,0 @@
-local teleportToBox = game:GetService("ReplicatedStorage"):WaitForChild("TeleportEvent")
-
-script.Parent.Activated:Connect(function()
-	teleportToBox:FireServer(script.Parent.Name)
-end)
